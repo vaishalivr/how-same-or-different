07ba1e89672c28013118d98d820508922716eae9
Vaishali Verma <65554439+vaishalivr@users.noreply.github.com>
2024-07-30 21:48:00 -0400

    adding title

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,39 @@
 <script>
-	export let name;
+  import TitleContainer from "./titleContainer.svelte";
+  import ScrollTitle from "./scrollTitle.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <ScrollTitle />
+  <!-- <TitleContainer /> -->
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :global(html, body) {
+    margin: 0;
+    padding: 0;
+    /* width: 100%; */
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  /* h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+    border: 1px solid black;
+    display: inline-block;
+  } */
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
